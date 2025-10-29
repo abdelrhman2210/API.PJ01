85dd02f1a85180a08737015b6cc06062b7a70ad7
abdelrhman2210 <abdelrhmanessam2210@gmail.com>
2025-10-29 13:35:06 +0300

    Session03-Vid02 (Picture Url Resolver)

---
+++ Core/API_PJ01_Services/Mapping/Products/ProductPictureUrlResolver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API_PJ01_Domain.Entities.Products;
+using API_PJ01_Shared.Dtos.Products;
+using AutoMapper;
+using Microsoft.Extensions.Configuration;
+
+namespace API_PJ01_Services.Mapping.Products
+{
+    public class ProductPictureUrlResolver(IConfiguration configuration) : IValueResolver<Product, ProductResponse, string>
+    {
+        public string Resolve(Product source, ProductResponse destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return $"{configuration["BaseUrl"]}/{source.PictureUrl}";
+            }
+            return string.Empty;
+        }
+    }
+}
--- Core/API_PJ01_Services/Mapping/Products/ProductProfile.cs
+++ Core/API_PJ01_Services/Mapping/Products/ProductProfile.cs
@@ -6,16 +6,19 @@ using System.Threading.Tasks;
 using API_PJ01_Domain.Entities.Products;
 using API_PJ01_Shared.Dtos.Products;
 using AutoMapper;
+using Microsoft.Extensions.Configuration;
 
 namespace API_PJ01_Services.Mapping.Products
 {
     public class ProductProfile : Profile
     {
-        public ProductProfile()
+        public ProductProfile(IConfiguration configuration)
         {
             CreateMap<Product, ProductResponse>()
                 .ForMember(D => D.Brand, O => O.MapFrom(S => S.Brand.Name))
-                .ForMember(D => D.Type, O => O.MapFrom(S => S.Type.Name));
+                .ForMember(D => D.Type, O => O.MapFrom(S => S.Type.Name))
+                //.ForMember(D => D.PictureUrl, O => O.MapFrom(S => $"{configuration["BaseUrl"]}/{S.PictureUrl}"));
+                .ForMember(D => D.PictureUrl, O => O.MapFrom(new ProductPictureUrlResolver(configuration)));
 
             CreateMap<ProductBrand, BrandTypeResponse>();
             CreateMap<ProductType, BrandTypeResponse>();
