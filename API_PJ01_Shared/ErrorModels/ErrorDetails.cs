2263c4f18b3f67c80fc12c8acb62d5a895cf8ac8
abdelrhman2210 <abdelrhmanessam2210@gmail.com>
2025-11-02 17:59:35 +0200

    Videos -> 1/2 Session 04

---
+++ API_PJ01_Shared/ErrorModels/ErrorDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_PJ01_Shared.ErrorModels
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
