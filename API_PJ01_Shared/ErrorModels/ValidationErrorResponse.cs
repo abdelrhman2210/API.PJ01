25d6b1ccefd8bfcb116a31d80c0e39f2b6bf238a
abdelrhman2210 <abdelrhmanessam2210@gmail.com>
2025-11-02 19:14:21 +0200

    V05 --> Validation Error Handling

---
+++ API_PJ01_Shared/ErrorModels/ValidationErrorResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace API_PJ01_Shared.ErrorModels
+{
+    public class ValidationErrorResponse
+    {
+        public int StatusCode { get; set; } = StatusCodes.Status400BadRequest;
+        public string ErrorMessage { get; set; } = "Validation Errors !!";
+        public IEnumerable<ValidationError> Errors { get; set; }
+    }
+
+    public class ValidationError
+    {
+        public string Field { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
