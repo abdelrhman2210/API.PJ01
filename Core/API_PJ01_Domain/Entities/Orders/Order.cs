91635e0f826c5ad25814d605ffed9e015be25066
abdelrhman2210 <abdelrhmanessam2210@gmail.com>
2025-11-22 12:16:22 +0200

    Order Module (DAL)

---
+++ Core/API_PJ01_Domain/Entities/Orders/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_PJ01_Domain.Entities.Orders
+{
+    public class Order : BaseEntity<Guid>
+    {
+        public Order()
+        {
+
+        }
+        public Order(string userEmail, OrderAddress shippingAddress, DeliveryMethod deliveryMethod, ICollection<OrderItem> items, decimal subTotal)
+        {
+            UserEmail = userEmail;
+            ShippingAddress = shippingAddress;
+            DeliveryMethod = deliveryMethod;
+            Items = items;
+            SubTotal = subTotal;
+        }
+
+        public string UserEmail { get; set; }
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public OrderAddress ShippingAddress { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; } // Navigation property
+        public int DeliveryMethodId { get; set; } // Foreign key
+        public ICollection<OrderItem> Items { get; set; } // Navigation property
+        public decimal SubTotal { get; set; } // Price * Quantity of all items
+        public decimal GetTotal() => SubTotal + DeliveryMethod.Price; // not mapped
+    }
+}
--- Infrastructure/API_PJ01_Persistence/Data/Contexts/StoreDbContext.cs
+++ Infrastructure/API_PJ01_Persistence/Data/Contexts/StoreDbContext.cs
@@ -1,11 +1,12 @@
-﻿using API_PJ01_Domain.Entities.Products;
-using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using API_PJ01_Domain.Entities.Orders;
+using API_PJ01_Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
 
 namespace API_PJ01_Persistence.Data.Contexts
 {
@@ -18,6 +19,10 @@ namespace API_PJ01_Persistence.Data.Contexts
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductBrand> ProductBrands { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
